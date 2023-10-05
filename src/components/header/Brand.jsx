f2f8c8cf2f3b9fe663802ecb576f1600f81ae2a9
lucianacaceresx <a.luciana.caceres@gmail.com>
2023-10-05 16:44:48 -0300

    arreglar estilo del logo

--- src/components/header/Brand.jsx
+++ src/components/header/Brand.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const Brand = () => {
     return(
         <Link to="/">
-            <img src={logo} alt="Tienda Hard" title="Tienda Hard"></img>
+            <img src={logo} alt="Tienda Hard" title="Tienda Hard" style={{ width: '100px', height: 'auto' }}></img>
         </Link>
     )
 }
